980aa7f10f51db5245f20756ede31b19d8a8f74a
ChienTQ2003 <anhchienbs.1@gmail.com>
2024-01-04 19:06:52 +0700

    first commit

---
+++ ExamAPI/src/main/java/com/example/examapi/ExamApiApplication.java
@@ -0,0 +1,14 @@
+package com.example.examapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamApiApplication {
+
+    public static void main(String[] args) {
+        System.out.println("anh chien dz vcl");
+        SpringApplication.run(ExamApiApplication.class, args);
+    }
+
+}
